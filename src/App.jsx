6826e9ed29aac14bbe1ef04557412b406d4769d7
Pete Seeling <peteseeling@gmail.com>
2022-04-26 13:48:45 -0600

    fetch characters written and passing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import { getCharactersFromApi, thenGetCharacters, asyncTryCatchCharacters, fetchCharacters } from "./services/promise-me";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+    <button onClick={async () => console.log(await    getCharactersFromApi())}>
+    GetCharacters
+    </button>
+
+    <button onClick={async () => console.log(await thenGetCharacters())}>
+    Then Get Characters
+</button>
+
+<button onClick={async () => console.log(await asyncTryCatchCharacters())}>
+    Try Catch Characters
+</button>
+
+<button onClick={async () => console.log(await fetchCharacters())}>
+    Fetch Characters
+</button>
+    
+    
+    </>
+  )
+
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,18 +1,58 @@
 /**
  * TODO: Exercise 1: use `fetch` & async/await to get data from an API
  */
+export async function getCharactersFromApi(){
+    const res = await fetch('https://futuramaapi.herokuapp.com/api/v2/characters');
+    const result = await res.json();
+    const characters = result[0];
+    const totalResults = result.length;
+
+    return { characters, totalResults };
+}
 
 /**
  * TODO: Exercise 2: use `fetch` & `.then` syntax to get the same data from the first exercise.
  */
+export function thenGetCharacters(){
+    return fetch('https://futuramaapi.herokuapp.com/api/v2/characters')
+        .then((res) => res.json())
+        .then((result) => ({ characters: result[0], totalResults: result.length }));
+
+}
 
 /**
  * TODO: Exercise 3: use `fetch`, async/await, and a try/catch statement to get data from an API and handle errors
  */
 
+export async function asyncTryCatchCharacters(){
+    try{
+        const res = await fetch('https://futuramaapi.herokuapp.com/api/v2/characters');
+        const result = await res.json();
+
+        return result
+
+    } catch (error){
+        console.log('Error occurred')
+    }
+
+
+    }
+
+
 /**
  * TODO: Exercise 4: use `fetch`, `.then`, and `.catch` to get the same data from exercise 3 while handling errors
  */
+
+export async function fetchCharacters(){
+try{
+    return fetch('https://futuramaapi.herokuapp.com/api/v2/characters')
+ .then((res) => res.json())
+ .then((result) => ({ characters: result[0], totalResults: result.length }));
+
+}catch (error){
+    console.log('Fetching Error')
+}
+}
 
 /**
  * TODO: Exercise 5: use `fetch`, async/await, and a try/catch/finally statement to get data from an API, handle errors, then console.log('All done!') upon completion
